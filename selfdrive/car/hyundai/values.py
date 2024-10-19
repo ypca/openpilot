d08175eaabfd869056a388e5e21b038464bd7f4a
Pablo C <46059474+ypca@users.noreply.github.com>
2024-10-18 22:02:48 -0700

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -320,7 +320,7 @@ class CAR(Platforms):
   )
   HYUNDAI_SONATA_HEV_2024 = HyundaiCanFDPlatformConfig(
     [HyundaiCarDocs("Hyundai Sonata Hybrid 2024", "All", car_parts=CarParts.common([CarHarness.hyundai_a]))],
-    CarSpecs(mass=1672, wheelbase=2.84, steerRatio=13.27 * 1.15, tireStiffnessFactor=0.65),
+    CarSpecs(mass=1672, wheelbase=2.84, steerRatio=12.68, tireStiffnessFactor=0.65),
     flags=HyundaiFlags.HYBRID,
   )
   HYUNDAI_IONIQ_5 = HyundaiCanFDPlatformConfig(
